a580dded62885e67dbef46c3447def6f2c034e3a
chanhee <bawoo1029@naver.com>
2019-10-30 17:26:43 +0900

    modified:   pyEVM.py

--- pyEVM.py
+++ pyEVM.py
@@ -1,13 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 필요한 라이브러리 및 모듈
+
 import numpy as np
 import cv2
 import butter
 from matplotlib import pyplot as plt
+
+
+# In[2]:
+
 
 # 1. Read Video file
 
 # generate video object
 filename = "./input/original_video_SR30.mp4"
 cap = cv2.VideoCapture(filename)
+
+
+# In[3]:
+
 
 # 2. Gray Scale로 변환
 
@@ -20,72 +36,105 @@ print("Shape of image is : ",frame.shape)
 
 # Gray Sacle 이미지로 변환
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+print("Shape of image is : ",gray.shape)
 
-# 4. Pyramid
-# 한 레벨에 대해 먼저 해보기
 
-Gau = [gray]
-Down = cv2.pyrDown(gray)
-Gau.append(Down)
+# ### Histogram 과 CLAHE
 
-for i in range (0, 5):
-    Down = cv2.pyrDown(Down)
-    Gau.append(Down)
+# In[4]:
 
-Gau_copy = []
-Gau_copy[:] = Gau
-Gau_copy.reverse()
 
-# 이미지를 sequential하게 저장
-Gau_seq = [[],[],[],[],[],[]]
-for i in range (0,6):
-    Gau_seq[i].append(Gau_copy.pop())
-    
-Lap = []
-for i in range (0, 6):
-    Up = cv2.pyrUp(Gau.pop())
-    Lap.append(Up - Gau[len(Gau)-1])
-    
-#Lap.reverse()
+# contrast limit가 2이고 title의 size는 8X8
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4,4))
+img2 = clahe.apply(gray)
+plt.subplot(211),plt.imshow(gray, cmap = 'gray'),plt.title('Original')
+plt.subplot(212),plt.imshow(img2, cmap = 'gray'),plt.title('CLAHE')
+plt.show()
 
-Lap_seq = [[],[],[],[],[],[]]
-for i in range (0,6):
-    Lap_seq[i].append(Lap.pop())
 
-# 모든 프레임에 대해 적용
+# In[5]:
+
+
+def CLAHE(input):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    output = clahe.apply(input)
+    return output
+
+
+# ### Gaussian pyramid
+# In a Gaussian pyramid, subsequent images are **weighted down using a Gaussian average (Gaussian blur) and scaled down.** Each pixel containing a local average corresponds to a neighborhood pixel on a lower level of the pyramid. This technique is used especially in texture synthesis.
+# 
+# ![image.png](attachment:image.png)
+# 
+# ### Laplacian pyramid
+# A Laplacian pyramid is very similar to a Gaussian pyramid but saves the difference image of the blurred versions between each levels. Only the smallest level is not a difference image to enable reconstruction of the high resolution image using the difference images on higher levels. This technique can be used in image compression
+# 
+# __출처 : https://en.wikipedia.org/wiki/Pyramid_(image_processing)__
+
+# In[6]:
+
+
+# pyramid 안쓰고, resize함수로 피라미드 정의
+
+def pyramidDown(input):
+    heigh, width = input.shape
+    blur = cv2.GaussianBlur(input, (5,5), 0)
+    output = cv2.resize(blur, None, fx = 0.5, fy = 0.5)
+    return output
+
+def pyramidUp(input):
+    heigh, width = input.shape
+    input = cv2.resize(input, None, fx = 2, fy = 2, interpolation = cv2.INTER_LINEAR)
+    output = cv2.GaussianBlur(input, (5,5), 0)
+    return output
+
+
+# In[7]:
+
+
+# 3. Generate Pyramid
+Gau_seq = [[],[],[],[],[],[],[]]
+Lap_seq = [[],[],[],[],[],[],[]]
 
 while ret:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = CLAHE(gray)
     
-    #hist = cv2.equalizeHist(gray)
+    # Normalize intensity
+    gray = gray.astype('float64')
+    gray = gray / 255
     
     Gau = [gray]
-    Down = cv2.pyrDown(gray)
+    Down = pyramidDown(gray)
     Gau.append(Down)
 
-    for i in range (0, 5):
-        Down = cv2.pyrDown(Down)
+    for i in range (0,6):
+        Down = pyramidDown(Down)
         Gau.append(Down)
     
     Gau_copy = []
     Gau_copy[:] = Gau
     Gau_copy.reverse()
     
-    for i in range (0,6):
+    for i in range (0,7):
         Gau_seq[i].append(Gau_copy.pop())
 
     Lap = []
-    for i in range (0, 6):
-        Up = cv2.pyrUp(Gau.pop())
-        Lap.append(Up - Gau[len(Gau)-1])
+    for i in range (0, 7):
+        Up = pyramidUp(Gau.pop())
+        Lap.append(Gau[len(Gau)-1]-Up)
 
     #Lap.reverse()
     
-    for i in range (0,6):
+    for i in range (0,7):
         Lap_seq[i].append(Lap.pop())
     ret, frame = cap.read()
 
-# 5. 필터링 및 각 진동 확대
+
+# In[8]:
+
+
+# 4. 확대 주파수 결정 및 필터링
 # Parameter Initializing 
 
 SR = 2200 # SR is Sampling Rate
@@ -93,106 +142,144 @@ Fn = SR/2
 T = 1/SR # T is Period
 Low_freq = 350
 High_freq = 370
-alpha = 10.0
+alpha = 200
 
-# 연산을 위해 부동소수점으로 변환
 
-for i in range(0, 6):
-    for j in range(0, 101):
-        Lap_seq[i][j] = Lap_seq[i][j].astype('float64')
+# In[9]:
+
 
 # 원하는 주파수 선택적으로 확대 
 import time
 start = time.time()
+#f=open('python_bandpass.csv','a')
+
+frame_size = Lap_seq[0][0].shape
 
 # level 0
 for i in range (0, frame_size[0]):
     for j in range (0, frame_size[1]):
         time_stack = []
         
-        for k in range (0, 101):
+        for k in range (0, 100):
             time_stack.append(Lap_seq[0][k][i][j])
             
         time_stack = butter.butter_bandpass_filter(time_stack, Low_freq, High_freq, SR, order=5)
-        
-        for k in range (0, 101):
+        #np.savetxt(f, np.column_stack(time_stack), delimiter=",", fmt='%s',  newline='\n')
+        for k in range (0, 100):
             Lap_seq[0][k][i][j] += alpha * time_stack[k]
             
 end = time.time()
 delay = end - start
 print("1st level : ", delay)
 
+
+# In[10]:
+
+
 # level 1 
 start = time.time()
+test = Lap_seq[1][0].copy()
 for i in range (0, Lap_seq[1][0].shape[0]):
     for j in range (0, Lap_seq[1][0].shape[1]):
         time_stack = []
         
-        for k in range (0, 101):
+        for k in range (0, 100):
             time_stack.append(Lap_seq[1][k][i][j])
             
         time_stack = butter.butter_bandpass_filter(time_stack, Low_freq, High_freq, SR, order=5)
         
-        for k in range (0, 101):
+        for k in range (0, 100):
             Lap_seq[1][k][i][j] += alpha * time_stack[k]
             
 end = time.time()
 delay = end - start
 print("2nd level : ", delay)
 
+
+# In[11]:
+
+
 # level 2
 start = time.time()
-
 for i in range (0, Lap_seq[2][0].shape[0]):
     for j in range (0, Lap_seq[2][0].shape[1]):
         time_stack = []
         
-        for k in range (0, 101):
+        for k in range (0, 100):
             time_stack.append(Lap_seq[2][k][i][j])
             
         time_stack = butter.butter_bandpass_filter(time_stack, Low_freq, High_freq, SR, order=5)
         
-        for k in range (0, 101):
+        for k in range (0, 100):
             Lap_seq[2][k][i][j] += time_stack[k]
             
 end = time.time()
 delay = end - start
 print("3nd level : ", delay)
 
-# 변환했던 부동소수점 데이터를 다시 정수로 변환
 
-for i in range(0, 6):
-    for j in range(0, 101):
-        Lap_seq[i][j] = Lap_seq[i][j].astype('uint8')
+# In[12]:
 
-# 5. 동영상으로 저장하기
-# 영상 스택 쌓기
 
 Rec2= []
-for i in range (0, 101):
-    Rec2.append( Lap_seq[2][i] + Gau_seq[2][i])
+for i in range (0, 100):
+    Sum = Lap_seq[2][i] + Gau_seq[2][i]
+    Rec2.append(Sum)
 
-for i in range (0, 101):
-    Rec2[i] = cv2.pyrUp(Rec2[i])
+for i in range (0, 100):
+    Rec2[i] = pyramidUp(Rec2[i])
 
 Rec1 = []
-for i in range (0, 101):
-    Rec1.append( Lap_seq[1][i] + Rec2[i] )
-
-for i in range (0, 101):
-    Rec1[i] = cv2.pyrUp(Rec1[i])
+for i in range (0, 100):
+    Sum = Lap_seq[1][i] + Rec2[i]
+    #Sum = Lap_seq[1][i] + Gau_seq[1][i]
+    Rec1.append(Sum)
+    
+for i in range (0, 100):
+    Rec1[i] = pyramidUp(Rec1[i])
 
 Rec0 = []
-for i in range (0, 101):
-    Rec0.append( Lap_seq[0][i] + Rec1[i] )
+for i in range (0, 100):
+    Sum = Lap_seq[0][i]+ Rec1[i]
+    Rec0.append(Sum)
 
-# Output 파일 생성 - 문제있음
-# 코덱문제인지? 동영상이 재생이 안됨
 
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter('./output/magnified.avi', fourcc, 30 , (256,640))
+# In[13]:
 
-for i in range (0, 101):
+
+# 형변환 및 오버플로우 방지
+
+for i in range(0, 100):
+    Rec0[i][Rec0[i]<0] = 0
+    Rec0[i][Rec0[i]>1] = 1
+    Rec0[i] *= 255
+    Rec0[i] = Rec0[i].astype('uint8')
+
+
+# In[14]:
+
+
+# Output 파일 생성 
+
+size = Rec0[0].shape
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter('./output/' + 'alpha_' + str(alpha) + 'output.mp4', fourcc, 30.0, (640, 256), isColor = False)
+
+for i in range (0, len(Rec0)):
     out.write(Rec0[i])
-
+    
 out.release()
+
+
+# In[15]:
+
+
+(Lap_seq[1][0]-test).max()
+
+
+# In[ ]:
+
+
+
+
